b64b67958f7f07be1f02e218fd53d6eb89430686
D3SXX <9647752d@gmail.com>
2025-07-10 00:09:33 +0300

    Remove unused imports

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,4 @@
-import {initBasic} from "./pages/api/utils/initData"
 import { fetcherPOST } from "./app/utils/fetcher"
-import Debug from "./pages/api/utils/debug"
-import { networkInterfaces } from 'os'
 
 export async function register() {
 
